9208be33d787a5bf20628db0972ba3f8d0080a03
Aporoid <Aporoid@hotmail.com>
2020-05-06 15:38:51 -0500

    starting scripting

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST };
+
+public class BattleSystem : MonoBehaviour
+{
+
+    public BattleState state;
+
+    public GameObject enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        Instantiate(enemyPrefab);
+    }
+
+}
